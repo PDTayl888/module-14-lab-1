05600a13488f225fde71aa8b067bbc475feca5c3
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-24 22:38:17 -0400

    added server

---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('../routes/userRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+app.use(express.json());
+
+app.use('api/users', userRoutes);
+
+async function connectDB() {
+    try {
+       await mongoose.connect(process.env.MONGO_URI)
+       console.log('MongoDB Connected!')
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+connectDB()
