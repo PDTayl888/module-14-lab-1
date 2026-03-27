071bc55b9f8946c673d16a343d878504adbd10a0
Patrick D. Taylor <patrickdtaylor1980@gmail.com>
2026-03-27 19:55:32 -0400

    debug

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ const PORT = process.env.PORT || 3030;
 
 app.use(express.json());
 
-app.use('api/users', userRoutes);
+app.use('/api/users', userRoutes);
 
 async function connectDB() {
     try {
        await mongoose.connect(process.env.MONGO_URI)
-       console.log('MongoDB Connected!')
+       app.listen(PORT, () => {
+                  console.log(`MONGODB CONNECTED AT PORT ${PORT}`);
+        });
     } catch(err) {
         console.log(err)
     }
